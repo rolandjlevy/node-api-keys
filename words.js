b7efcd0ec29fce9a1e140d854a9426509e7221a4
Roland Levy <rolandjlevy@gmail.com>
2020-10-11 00:19:04 +0000

    Updated .env

--- words.js
+++ words.js
@@ -2,6 +2,8 @@ const words = [
   'fruit', 'vegetables', 'book', 'pencil', 'paper', 'pen', 'laptop', 'website', 'phone', 'house', 'flat', 'garden', 'tree', 'flower', 'plant', 'shop', 'food', 'friend', 'person', 'dog', 'cat', 'cafe', 'restaurant', 'car', 'road', 'art', 'music', 'happy', 'beach', 'holiday', 'tree', 'river', 'mountain', 'bike', 'fitness', 'freedom', 'peace', 'meditation'
 ];
 
+words.sort();
+
 const options = words.map(word => `<option value="${word}">${word}</option>`).join('\n');
 
 module.exports = {
